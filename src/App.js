2da7c365d65ec401472698d1f69e6e4688de0488
bluememfis <mrshaunsharp@gmail.com>
2021-09-23 14:24:41 -0700

    first pig

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
     this.state = {
       // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "potato",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
       phraseTranslated: "This is where your translated sentence will appear."
     }
@@ -34,75 +34,85 @@ class App extends Component{
       console.log("vowelsArray:", vowelsArray)
 
       // your code here!
+      if(vowelsArray.indexOf(currentWord[0])){
+        let pigVowel = ['a','e','i','o','u'];
+        let newPig = "";
+        pigLatin = currentWord + "way";
+        return pigLatin;
+      } 
+    }
+  
+//       // else {
+//       //   let firstPig = currentWord.match(/[aeiou]/g)|| 0;
+//       //   newPig = currentWord
+// //       // Remember: console.log is your friend :)
 
-      // Remember: console.log is your friend :)
+
+// //       // ACTION ITEM: change the value of currentWord to the name of whatever variable you made containing your Pig Latin'd word
+// //       return currentWord
+// //     })
 
 
-      // ACTION ITEM: change the value of currentWord to the name of whatever variable you made containing your Pig Latin'd word
-      return currentWord
-    })
+//     // joining the array back to a string of translated words
+//     // no need to change this variable
+//     let translatedWords = translatedWordsArray.join(" ")
+//     console.log("translatedWords:", translatedWords)
 
+//     // the setState method will take your information from "translatedWords" and update the state object that is displayed to the user
+//     // no need to change this method
+//     this.setState({phraseTranslated: translatedWords})
+//   }
 
-    // joining the array back to a string of translated words
-    // no need to change this variable
-    let translatedWords = translatedWordsArray.join(" ")
-    console.log("translatedWords:", translatedWords)
+//   restartGame = () => {
+//     // this method restarts the game by setting the original state
+//     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
+//     this.setState({
+//       phrase: "alpha through yummy squeal queen fry",
+//       phraseTranslated: "This is where your translated sentence will appear."
+//     })
+//   }
 
-    // the setState method will take your information from "translatedWords" and update the state object that is displayed to the user
-    // no need to change this method
-    this.setState({phraseTranslated: translatedWords})
-  }
+//   // no need to modify this method
+//   setUpPreventDefault = (e) => {
+//     // this method prevents React from refreshing the page unnecessarily
+//     e.preventDefault()
+//     this.myPigLatinCodeHere()
+//   }
 
-  restartGame = () => {
-    // this method restarts the game by setting the original state
-    // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
-    this.setState({
-      phrase: "alpha through yummy squeal queen fry",
-      phraseTranslated: "This is where your translated sentence will appear."
-    })
-  }
+//   // no need to modify this method
+//   handleInput = (e) => {
+//     // this method takes the input and saves the value in this.state.phrase so we can use the input in our program
+//     this.setState({phrase: e.target.value})
+//   }
 
-  // no need to modify this method
-  setUpPreventDefault = (e) => {
-    // this method prevents React from refreshing the page unnecessarily
-    e.preventDefault()
-    this.myPigLatinCodeHere()
-  }
+//   render() {
+//     return (
+//       <>
+//         <h1>Pig Latin Translator</h1>
+//         <img
+//           src={butcherPig}
+//           alt="pig with butcher cut names in pig latin"
+//           className="butcherPig"
+//         />
+//         <div className="inputArea">
+//           <h4>Enter phrase to be translated:</h4>
+//           {/* user input field - every DOM event that happens in the input will call the handleChange method and update state */}
+//           <input
+//             type="text"
+//             className="userInput"
+//             onChange={this.handleInput}
+//             value={this.state.phrase}
+//           />
+//           <br />
+//           {/* button that called the setUpPreventDefault method which calls the myPigLatinCodeHere method */}
+//           <button onClick={this.setUpPreventDefault}>Submit</button>
+//           <button onClick={this.restartGame}>Clear</button>
+//         </div>
+//         <p>{this.state.phraseTranslated}</p>
+//         <footer>Coded by ~your name here~</footer>
+//       </>
+//     )
+//   }
+// }
 
-  // no need to modify this method
-  handleInput = (e) => {
-    // this method takes the input and saves the value in this.state.phrase so we can use the input in our program
-    this.setState({phrase: e.target.value})
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Pig Latin Translator</h1>
-        <img
-          src={butcherPig}
-          alt="pig with butcher cut names in pig latin"
-          className="butcherPig"
-        />
-        <div className="inputArea">
-          <h4>Enter phrase to be translated:</h4>
-          {/* user input field - every DOM event that happens in the input will call the handleChange method and update state */}
-          <input
-            type="text"
-            className="userInput"
-            onChange={this.handleInput}
-            value={this.state.phrase}
-          />
-          <br />
-          {/* button that called the setUpPreventDefault method which calls the myPigLatinCodeHere method */}
-          <button onClick={this.setUpPreventDefault}>Submit</button>
-          <button onClick={this.restartGame}>Clear</button>
-        </div>
-        <p>{this.state.phraseTranslated}</p>
-        <footer>Coded by ~your name here~</footer>
-      </>
-    )
-  }
-}
-
-export default App
+// export default App
